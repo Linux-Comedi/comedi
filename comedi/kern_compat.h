588f2de8816696465c3481d7ac196c7ccdb472b0
David Schleef <ds@schleef.org>
2000-07-13 01:29:23 +0000

    added module stuff for 2.0

--- comedi/kern_compat.h
+++ comedi/kern_compat.h
@@ -209,10 +209,20 @@ typedef struct wait_queue *wait_queue_head_t;
 #endif
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(2,3,0)		/* ? */
-#ifdef MODULE
-#define THIS_MODULE	&__this_module
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2,1,0)
+  #ifdef MODULE
+  /* don't ask.  It works. */
+  #define THIS_MODULE			((struct module *)&mod_use_count_)
+  #else
+  #define THIS_MODULE	NULL
+  #endif
+  #define __MOD_INC_USE_COUNT(x)	((*(long *)(x))++, (*(long *)(x)) |= MOD_VISITED)
+  #define __MOD_DEC_USE_COUNT(x)	((*(long *)(x))--, (*(long *)(x)) |= MOD_VISITED)
 #else
-#define THIS_MODULE	NULL
+  #ifdef MODULE
+  #define THIS_MODULE	&__this_module
+  #else
+  #define THIS_MODULE	NULL
 #endif
 #endif
 
