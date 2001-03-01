79cd58704121ce8aa6b849a649ebbc5b80f98688
David Schleef <ds@schleef.org>
2001-03-01 05:02:57 +0000

    added compatibility for kill_fasync()

---
+++ include/linux/fs.h
@@ -0,0 +1,19 @@
+/*
+ * linux/fs.h compatibility header
+ */
+
+#ifndef __COMPAT_LINUX_FS_H_
+#define __COMPAT_LINUX_FS_H_
+
+#include <linux/version.h>
+
+#if LINUX_VERSION_CODE < 0x020400
+#define KILL_FASYNC(a,b,c)	kill_fasync((a),(c))
+#else
+#define KILL_FASYNC(a,b,c)	kill_fasync(&(a),(b),(c))
+#endif
+
+#include_next <linux/fs.h>
+
+#endif
+
