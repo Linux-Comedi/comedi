77c7c05d583b5a2e75c9da45b8a42e9f2c52cb42
Frank Mori Hess <fmhess@speakeasy.net>
2001-09-09 21:38:40 +0000

    new compatibility header for *_mem_region() functions

---
+++ include/linux/ioport.h
@@ -0,0 +1,19 @@
+// linux/ioport.h compatibility header
+
+#ifndef _COMPAT_IOPORT_H
+#define _COMPAT_IOPORT_H
+
+#include <linux/version.h>
+
+#if KERNEL_VERSION_CODE < KERNEL_VERSION(2,3,17)
+
+#define check_mem_region(start,n) 0
+#define request_mem_region(start,ni,name) 0
+#define release_mem_region(start,n) 0
+
+#endif
+
+#include_next <linux/ioport.h>
+
+#endif // _COMPAT_IOPORT_H
+
