57826302830091dec7c647c584ad2e9c4bc6066a
David Schleef <ds@schleef.org>
2001-09-13 00:47:11 +0000

    Fixed argument of pci_enable_dev()

--- comedi/drivers/rtd520.c
+++ comedi/drivers/rtd520.c
@@ -621,7 +621,7 @@ static int rtd_attach (
      */
     dev->board_name = thisboard->name;
 
-    if((ret=pci_enable_device(dev))<0){
+    if((ret=pci_enable_device(pcidev))<0){
 	return ret;
     }
 
