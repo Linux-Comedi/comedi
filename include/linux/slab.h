e962f044b6ca19309a48b853dbe639ed83bef237
David Schleef <ds@schleef.org>
2001-12-01 00:34:53 +0000

    Linux-2.4 prefers slab.h

---
+++ include/linux/slab.h
@@ -0,0 +1,17 @@
+/*
+ * linux/slab.h compatibility header
+ */
+
+#ifndef __COMPAT_LINUX_SLAB_H_
+#define __COMPAT_LINUX_SLAB_H_
+
+#include <linux/version.h>
+
+#if LINUX_VERSION_CODE < 0x020400
+#include <linux/malloc.h>
+#else
+#include_next <linux/slab.h>
+#endif
+
+#endif
+
