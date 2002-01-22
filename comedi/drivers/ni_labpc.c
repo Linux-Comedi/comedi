24c8007f9447d8ef6cbe0455481b0e81a767c879
Frank Mori Hess <fmhess@speakeasy.net>
2002-01-22 03:08:31 +0000

    fixing device names for pcmcia

--- comedi/drivers/ni_labpc.c
+++ comedi/drivers/ni_labpc.c
@@ -2515,7 +2515,7 @@ static void labpc_config(dev_link_t *link)
       At this point, the dev_node_t structure(s) need to be
       initialized and arranged in a linked list at link->dev.
     */
-    sprintf(dev->node.dev_name, "skel0");
+    sprintf(dev->node.dev_name, "ni_labpc");
     dev->node.major = dev->node.minor = 0;
     link->dev = &dev->node;
 
