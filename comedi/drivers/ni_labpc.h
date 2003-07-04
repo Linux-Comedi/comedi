a780716460b30897b22840beeb0e819d4c7a1708
Frank Mori Hess <fmhess@speakeasy.net>
2003-07-04 20:00:26 +0000

    split PCMCIA support out of das08.o driver

--- comedi/drivers/ni_labpc.h
+++ comedi/drivers/ni_labpc.h
@@ -3,6 +3,8 @@
 
     Header for ni_labpc.c and ni_labpc_cs.c
 
+    Copyright (C) 2003 Frank Mori Hess <fmhess@users.sourceforge.net>
+
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
