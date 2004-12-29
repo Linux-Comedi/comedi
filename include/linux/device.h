45e15e404308ad4e2f7d00e91eb6b381397f96cb
Frank Mori Hess <fmhess@speakeasy.net>
2004-12-29 03:10:46 +0000

    compatibility functions so udev support compiles on 2.4 kernels

--- include/linux/device.h
+++ include/linux/device.h
@@ -14,6 +14,31 @@ struct device_driver
 {
 	dev_info_t name;
 };
+
+typedef void class_simple;
+
+struct device
+{
+	void *dummy;
+};
+
+static inline struct class_simple *class_simple_create(struct module *owner, char *name)
+{
+	return NULL;
+};
+
+static inline void class_simple_destroy(struct class_simple *cs)
+{};
+
+static inline struct class_device *class_simple_device_add(struct class_simple *cs, 
+	dev_t dev, struct device *device, const char *fmt, ...)
+{
+	return NULL;
+};
+
+static inline void class_simple_device_remove(dev_t dev)
+{};
+
 #else
 #include_next <linux/device.h>
 #endif
