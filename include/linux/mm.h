0ffd0d30504095732759a92b0e08994feccb9f49
Frank Mori Hess <fmhess@speakeasy.net>
2005-02-27 23:31:17 +0000

    need to change things around a little to prevent asm/tlb.h from breaking compilation against debian 2.4.27 kernel headers

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -14,8 +14,11 @@
 #define VM_OFFSET(a)	((a)->vm_pgoff * PAGE_SIZE)
 #endif
 
+#include_next <linux/mm.h>
+
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(2,4,10) \
 			  && LINUX_VERSION_CODE < KERNEL_VERSION(2,5,3)
+#include <asm/pgalloc.h>
 #include <asm/tlb.h>	/* look for tlb_vma() macro for "statm" patch */
 #endif
 
@@ -24,8 +27,6 @@
 #else
 #define REMAP_PAGE_RANGE(a,b,c,d,e) remap_page_range(a,b,c,d,e)
 #endif
-
-#include_next <linux/mm.h>
 
 #endif /* _COMPAT_MM_H */
 
