910b74c4329221458a3dc3a76773df71b42558fe
David Schleef <ds@schleef.org>
2005-04-02 23:52:27 +0000

    Use autoreconf.  Add files

--- autogen.sh
+++ autogen.sh
@@ -1,20 +1,5 @@
 #!/bin/sh
-if [ ! -f NEWS ] ; then
-        touch NEWS
-fi
-if [ ! -f AUTHORS ] ; then
-        touch AUTHORS
-fi
-if [ ! -f ChangeLog ] ; then
-        touch ChangeLog
-fi
 
-aclocal -I m4
-autoheader
-autoconf
-automake --add-missing --copy
-
-rm -f config.cache
-
+autoreconf &&
 ./configure --enable-maintainer-mode $*
 
