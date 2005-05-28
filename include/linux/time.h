3ffab58232a24c121b021f624c13ee9b0dd3d1c5
Frank Mori Hess <fmhess@speakeasy.net>
2005-05-28 19:02:24 +0000

    added jiffies_to_msecs() and msleep_interruptible() to compatibility headers

--- include/linux/time.h
+++ include/linux/time.h
@@ -7,14 +7,26 @@
 #include_next <linux/time.h>
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,0)
+
+static inline unsigned int jiffies_to_msecs(const unsigned long j)
+{
+#if HZ <= 1000 && !(1000 % HZ)
+	return (1000 / HZ) * j;
+#elif HZ > 1000 && !(HZ % 1000)
+	return (j + (HZ / 1000) - 1)/(HZ / 1000);
+#else
+	return (j * 1000) / HZ;
+#endif
+}
+
 static inline unsigned long msecs_to_jiffies(const unsigned int m)
 {
 #if HZ <= 1000 && !(1000 % HZ)
-		return (m + (1000 / HZ) - 1) / (1000 / HZ);
+	return (m + (1000 / HZ) - 1) / (1000 / HZ);
 #elif HZ > 1000 && !(HZ % 1000)
-		return m * (HZ / 1000);
+	return m * (HZ / 1000);
 #else
-		return (m * HZ + 999) / 1000;
+	return (m * HZ + 999) / 1000;
 #endif
 };
 
