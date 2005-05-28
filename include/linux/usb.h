4895709d3cce87eb03465ca26c4dca2cd37bafe6
Frank Mori Hess <fmhess@speakeasy.net>
2005-05-28 19:10:32 +0000

    * include/linux/delay.h: Added msleep_interruptible() * include/linux/time.h:i Added msecs_to_jiffies() and jiffies_to_msecs() * include/linux/usb.h: Added USB_CONTROL_MSG() and USB_BULK_MSG()

--- include/linux/usb.h
+++ include/linux/usb.h
@@ -24,6 +24,7 @@
 #define __COMPAT_LINUX_USB_H_
 
 #include <linux/version.h>
+#include <linux/time.h>
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,0)
 #define USB_ALLOC_URB(x) usb_alloc_urb(x)
@@ -38,6 +39,25 @@
 
 
 #include_next <linux/usb.h>
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,12)
+static inline int USB_CONTROL_MSG(struct usb_device *dev, unsigned int pipe,
+	__u8 request, __u8 requesttype, __u16 value, __u16 index,
+	void *data, __u16 size, int millisec_timeout)
+{
+	return usb_control_msg(dev, pipe, request, requesttype, value, index,
+		data, size, msecs_to_jiffies(millisec_timeout));
+}
+static inline int USB_BULK_MSG(struct usb_device *usb_dev, unsigned int pipe,
+	void *data, int len, int *actual_length,
+	int millisec_timeout)
+{
+	return usb_bulk_msg(usb_dev, pipe, data, len, actual_length, msecs_to_jiffies(millisec_timeout));
+}
+#else
+#define USB_CONTROL_MSG usb_control_msg
+#define USB_BULK_MSG usb_bulk_msg
+#endif
 
 #endif
 
