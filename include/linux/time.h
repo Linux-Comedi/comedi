c33ec61a4bd3c66f8810a15821bee4db1d348daa
Frank Mori Hess <fmhess@speakeasy.net>
2005-05-28 18:54:31 +0000

    added msecs_to_jiffies() compatibility definition

---
+++ include/linux/time.h
@@ -0,0 +1,24 @@
+
+#ifndef __COMPAT_LINUX_TIME_H
+#define __COMPAT_LINUX_TIME_H
+
+#include <linux/version.h>
+
+#include_next <linux/time.h>
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,0)
+static inline unsigned long msecs_to_jiffies(const unsigned int m)
+{
+#if HZ <= 1000 && !(1000 % HZ)
+		return (m + (1000 / HZ) - 1) / (1000 / HZ);
+#elif HZ > 1000 && !(HZ % 1000)
+		return m * (HZ / 1000);
+#else
+		return (m * HZ + 999) / 1000;
+#endif
+};
+
+#endif
+
+#endif
+
