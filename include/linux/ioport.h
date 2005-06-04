be3c1c2bf0dd83072caae5cf82df39dedf75063e
Frank Mori Hess <fmhess@speakeasy.net>
2005-06-04 14:40:52 +0000

    Patch from abbotti@mev.co.uk (Ian Abbott) to make it possible to remove deprecated check_region() calls from drivers. * include/linux/ioport.h: Added compat__request_region() * include/linux/pci.h: Replace use of check_region() with modern version of request_region().

--- include/linux/ioport.h
+++ include/linux/ioport.h
@@ -15,5 +15,30 @@
 
 #include_next <linux/ioport.h>
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2,3,11)
+
+/* 'struct resource' not defined.  Define a dummy version. */
+struct resource {
+	int dummy;
+};
+
+/* Define a compatible version of request_region that returns a pointer
+ * to a non-NULL value on success.  */
+static inline struct resource *compat__request_region(unsigned long from,
+		unsigned long extent, const char *name)
+{
+	if (check_region(from, extent) < 0) {
+		return (struct resource *)0;
+	}
+	request_region(from, extent, name);
+	return ((struct resource *)0 + 1);	/* Should be non-NULL */
+}
+
+/* Replace existing request_region macro/function. */
+#undef request_region
+#define request_region(f,e,n)	compat__request_region(f,e,n)
+
+#endif
+
 #endif // _COMPAT_IOPORT_H
 
