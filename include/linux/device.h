dfa30a104b09100002f4dbc164500e47572038ba
Frank Mori Hess <fmhess@speakeasy.net>
2006-09-14 17:52:06 +0000

    Added GPL header.

--- include/linux/device.h
+++ include/linux/device.h
@@ -1,6 +1,24 @@
 /*
  * linux/device.h compatibility header
  */
+/*
+    Copyright (C) 2004-2006 Frank Mori Hess <fmhess@users.sourceforge.net>
+    Copyright (C) 2005-2006 Ian Abbott
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
 
 #ifndef __COMPAT_LINUX_DEVICE_H_
 #define __COMPAT_LINUX_DEVICE_H_
@@ -10,7 +28,7 @@
 #if LINUX_VERSION_CODE < KERNEL_VERSION(2,5,0)
 #include <pcmcia/cs_types.h>
 
-struct device_driver 
+struct device_driver
 {
 	char *name;
 };
@@ -33,7 +51,7 @@ static inline struct class_device *COMEDI_CLASS_DEVICE_CREATE(struct class *cls,
 {
 	return NULL;
 };
- 
+
 static inline void class_device_destroy(struct class *cs, dev_t dev)
 {};
 
