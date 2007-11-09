81e9a5af8fe9b75cbba48a619042cf7844b86d46
Ian Abbott <abbotti@mev.co.uk>
2007-11-09 15:23:42 +0000

    In cmdtest, use DPRINTK instead of comedi_error.

--- comedi/drivers/amplc_pci224.c
+++ comedi/drivers/amplc_pci224.c
@@ -989,14 +989,16 @@ pci224_ao_cmdtest(comedi_device * dev, comedi_subdevice * s, comedi_cmd * cmd)
 		}
 		if (errors) {
 			if (errors & dupchan_err) {
-				comedi_error(dev,
+				DPRINTK("comedi%d: " DRIVER_NAME
+					": ao_cmdtest: "
 					"entries in chanlist must contain no "
-					"duplicate channels\n");
+					"duplicate channels\n", dev->minor);
 			}
 			if (errors & range_err) {
-				comedi_error(dev,
+				DPRINTK("comedi%d: " DRIVER_NAME
+					": ao_cmdtest: "
 					"entries in chanlist must all have "
-					"the same range index\n");
+					"the same range index\n", dev->minor);
 			}
 			err++;
 		}
