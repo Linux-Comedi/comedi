0e4888cb3c866847fb0933464797851c890e919c
Frank Mori Hess <fmhess@speakeasy.net>
2007-11-12 22:15:31 +0000

    Fixed sign error that prevented commands from aborting properly on errors.

--- comedi/drivers/ni_mio_common.c
+++ comedi/drivers/ni_mio_common.c
@@ -910,7 +910,7 @@ static void ni_event(comedi_device * dev, comedi_subdevice * s)
 	if (s->async->
 		events & (COMEDI_CB_ERROR | COMEDI_CB_OVERFLOW | COMEDI_CB_EOA))
 	{
-		switch (dev->subdevices - s) {
+		switch (s - dev->subdevices) {
 		case NI_AI_SUBDEV:
 			ni_ai_reset(dev, s);
 			break;
