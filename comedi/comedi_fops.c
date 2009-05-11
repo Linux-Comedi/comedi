d7031a8893c7319e3dc0454d6674ec754c75a374
Ian Abbott <abbotti@mev.co.uk>
2009-05-11 09:37:41 +0000

    Corrected type of a printk argument in resize_async_buffer().

--- comedi/comedi_fops.c
+++ comedi/comedi_fops.c
@@ -2340,7 +2340,7 @@ static int resize_async_buffer(comedi_device *dev,
 	}
 
 	DPRINTK("comedi%i subd %d buffer resized to %i bytes\n",
-		dev->minor, s - dev->subdevices, async->prealloc_bufsz);
+		dev->minor, (int)(s - dev->subdevices), async->prealloc_bufsz);
 	return 0;
 }
 
