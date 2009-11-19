6f4c04d2237aea4ff0f82dc7f4ebd94ba27f292b
Ian Abbott <abbotti@mev.co.uk>
2009-11-19 11:39:10 +0000

    pcl816: In pcl816_ai_cmdtest(), initialize divisor1 and divisor2 values passed by reference to i8253_cascase_ns_to_timer().

--- comedi/drivers/pcl816.c
+++ comedi/drivers/pcl816.c
@@ -462,7 +462,7 @@ static int pcl816_ai_cmdtest(comedi_device * dev, comedi_subdevice * s,
 	comedi_cmd * cmd)
 {
 	int err = 0;
-	int tmp, divisor1, divisor2;
+	int tmp, divisor1 = 0, divisor2 = 0;
 
 	DEBUG(rt_printk("pcl816 pcl812_ai_cmdtest\n");
 		pcl816_cmdtest_out(-1, cmd););
