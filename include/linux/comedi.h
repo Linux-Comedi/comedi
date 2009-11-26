3677e488e1d0b11dc1ef75f5719ceded8e275ad8
Bernd Porr <Bernd.Porr@f2s.com>
2009-11-26 20:18:59 +0000

    removed "deprecated" from the define COMEDI_CB_BLOCK and added a comment so that other know that this flag has to be set whenever fresh data has arrived from an A/D or has been written to a D/A.

--- include/linux/comedi.h
+++ include/linux/comedi.h
@@ -467,7 +467,7 @@ extern "C" {
 
 #define COMEDI_CB_EOS		1	/* end of scan */
 #define COMEDI_CB_EOA		2	/* end of acquisition */
-#define COMEDI_CB_BLOCK		4	/* DEPRECATED: convenient block size */
+#define COMEDI_CB_BLOCK		4	/* new data has arrived: wakes up write()/read() */
 #define COMEDI_CB_EOBUF		8	/* DEPRECATED: end of buffer */
 #define COMEDI_CB_ERROR		16	/* card error during acquisition */
 #define COMEDI_CB_OVERFLOW	32	/* buffer overflow/underflow */
