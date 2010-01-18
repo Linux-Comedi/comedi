04d149c33905138e2800fc98534299852ee11f0e
Ian Abbott <abbotti@mev.co.uk>
2010-01-18 19:38:39 +0000

    adv_pci1710: Don't need to check chanlist_len in pci171x_ai_cmdtest().

--- comedi/drivers/adv_pci1710.c
+++ comedi/drivers/adv_pci1710.c
@@ -1001,14 +1001,6 @@ static int pci171x_ai_cmdtest(comedi_device * dev, comedi_subdevice * s,
 		}
 	}
 
-	if (!cmd->chanlist_len) {
-		cmd->chanlist_len = 1;
-		err++;
-	}
-	if (cmd->chanlist_len > this_board->n_aichan) {
-		cmd->chanlist_len = this_board->n_aichan;
-		err++;
-	}
 	if (cmd->scan_end_arg != cmd->chanlist_len) {
 		cmd->scan_end_arg = cmd->chanlist_len;
 		err++;
