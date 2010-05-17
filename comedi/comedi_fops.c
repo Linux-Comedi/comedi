5e9f2e262e05fd8b1be3d72d63fd908d3ff48748
Ian Abbott <abbotti@mev.co.uk>
2010-05-17 11:26:07 +0000

    For COMEDI_BUFINFO ioctl, if subdevice has no async capability, set bytes_read and bytes_written to 0 in returned structure.

--- comedi/comedi_fops.c
+++ comedi/comedi_fops.c
@@ -594,6 +594,8 @@ static int do_bufinfo_ioctl(comedi_device * dev, void *arg)
 		bi.buf_read_ptr = 0;
 		bi.buf_write_count = 0;
 		bi.buf_read_count = 0;
+		bi.bytes_read = 0;
+		bi.bytes_written = 0;
 		goto copyback;
 	}
 
