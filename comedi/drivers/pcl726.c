09bf028e9547a133b61c7c08aed690e90194b4be
Ian Abbott <abbotti@mev.co.uk>
2013-08-29 14:48:07 +0100

    pcl726: correct insn_bits for DO subdevice
    The insn_bits handler for the digital output subdevice is filtering the
    register writes based on the data bits value in data[1], but it should
    be filtering on the mask value in data[0].  Fix it.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>

--- comedi/drivers/pcl726.c
+++ comedi/drivers/pcl726.c
@@ -226,11 +226,11 @@ static int pcl726_do_insn_bits(comedi_device * dev, comedi_subdevice * s,
 	if (data[0]) {
 		s->state &= ~data[0];
 		s->state |= data[0] & data[1];
+		if (data[0] & 0x00ff)
+			outb(s->state & 0xff, dev->iobase + this_board->do_lo);
+		if (data[0] & 0xff00)
+			outb((s->state >> 8), dev->iobase + this_board->do_hi);
 	}
-	if (data[1] & 0x00ff)
-		outb(s->state & 0xff, dev->iobase + this_board->do_lo);
-	if (data[1] & 0xff00)
-		outb((s->state >> 8), dev->iobase + this_board->do_hi);
 
 	data[1] = s->state;
 
