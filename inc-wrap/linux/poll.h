92ff7f3a9e7f973706a1fd7d7524802e93af2735
Ian Abbott <abbotti@mev.co.uk>
2018-07-04 17:15:58 +0100

    Create <linux/poll.h> compatibility header and define COMEDI_EPOLLIN etc.
    For kernel versions 4.16 onwards, the ->poll() file-operation should use
    the EPOLL* constants instead of the POLL* constants (e.g. EPOLLIN
    instead of POLLIN).  Define our own constants COMEDI_EPOLLIN etc. that
    expand to the correct constants depending on the kernel version.  It
    only makes a difference on those architectures where the EPOLL*
    constants have different values to the POLL* constants.
    
    Other comedi kernel code needs changing to use these constants.
    
    It only makes a difference except on blackfin, frv, m68k, mips, sparc,
    and xtensa architectures.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>

---
+++ inc-wrap/linux/poll.h
@@ -0,0 +1,48 @@
+/*
+ * linux/poll.h compatibility header
+ */
+
+#ifndef __COMPAT_LINUX_POLL_H_
+#define __COMPAT_LINUX_POLL_H_
+
+#include <linux/version.h>
+
+#include_next <linux/poll.h>
+
+/*
+ * For poll() file operation, the EPOLL* constants should be used for kernel
+ * versions 4.16 onwards, and the POLL* constants should be used for earlier
+ * kernel versions.
+ *
+ * Define our own COMEDI_EPOLL constants to select the ones appropriate for
+ * the kernel version.
+ */
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4,16,0)
+#define COMEDI_EPOLLIN		POLLIN
+#define COMEDI_EPOLLPRI		POLLPRI
+#define COMEDI_EPOLLOUT		POLLOUT
+#define COMEDI_EPOLLERR		POLLERR
+#define COMEDI_EPOLLHUP		POLLHUP
+#define COMEDI_EPOLLNVAL	POLLNVAL
+#define COMEDI_EPOLLRDNORM	POLLRDNORM
+#define COMEDI_EPOLLRDBAND	POLLRDBAND
+#define COMEDI_EPOLLWRNORM	POLLWRNORM
+#define COMEDI_EPOLLWRBAND	POLLWRBAND
+#define COMEDI_EPOLLMSG		POLLMSG
+#define COMEDI_EPOLLRDHUP	POLLRDHUP
+#else
+#define COMEDI_EPOLLIN		EPOLLIN
+#define COMEDI_EPOLLPRI		EPOLLPRI
+#define COMEDI_EPOLLOUT		EPOLLOUT
+#define COMEDI_EPOLLERR		EPOLLERR
+#define COMEDI_EPOLLHUP		EPOLLHUP
+#define COMEDI_EPOLLNVAL	EPOLLNVAL
+#define COMEDI_EPOLLRDNORM	EPOLLRDNORM
+#define COMEDI_EPOLLRDBAND	EPOLLRDBAND
+#define COMEDI_EPOLLWRNORM	EPOLLWRNORM
+#define COMEDI_EPOLLWRBAND	EPOLLWRBAND
+#define COMEDI_EPOLLMSG		EPOLLMSG
+#define COMEDI_EPOLLRDHUP	EPOLLRDHUP
+#endif
+
+#endif /* __COMPAT_LINUX_POLL_H */
