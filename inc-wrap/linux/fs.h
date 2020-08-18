231e61eb48d90e0c40207445bcb2c80f261b954e
Ian Abbott <abbotti@mev.co.uk>
2020-08-18 15:24:17 +0100

    inc-wrap/linux/fs.h: HAVE_UNLOCKED_IOCTL and HAVE_COMPAT_IOCTL fix for kernel version 5.9+
    The HAVE_UNLOCKED_IOCTL and HAVE_COMPAT_IOCTL macros have been defined by
    <linux/fs.h> since kernel version 2.6.11, but are being removed in
    kernel version 5.9 since no in-tree kernel source uses them.
    
    Comedi code still uses HAVE_UNLOCKED_IOCTL and HAVE_COMPAT_IOCTL, so
    ensure they are defined for any kernel version from 2.6.11 upwards.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>

--- inc-wrap/linux/fs.h
+++ inc-wrap/linux/fs.h
@@ -5,6 +5,18 @@
 #include <linux/version.h>
 #include_next <linux/fs.h>
 
+/*
+ * The HAVE_COMPAT_IOCTL and HAVE_UNLOCKED_IOCTL macros were defined in
+ * 2.6.11 but removed in 5.9 because nothing in the kernel source uses them,
+ * but we do.
+ */
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,11)
+#undef HAVE_COMPAT_IOCTL
+#define HAVE_COMPAT_IOCTL 1
+#undef HAVE_UNLOCKED_IOCTL
+#define HAVE_UNLOCKED_IOCTL 1
+#endif
+
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3,9,0)
 
 #undef file_inode
