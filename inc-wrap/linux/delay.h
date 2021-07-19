e114e1ce219693d08327bc2f30e4546eb7c2ab01
Ian Abbott <abbotti@mev.co.uk>
2021-07-19 16:44:15 +0100

    inc-wrap/linux/delay.h: Add compatibility wrappers for msleep() and ssleep()
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>

--- inc-wrap/linux/delay.h
+++ inc-wrap/linux/delay.h
@@ -7,6 +7,18 @@
 
 #include_next <linux/delay.h>
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,7)
+static inline void msleep(unsigned int msecs)
+{
+	unsigned long timeout = msecs_to_jiffies(msecs);
+
+	while (timeout) {
+		set_current_state(TASK_UNINTERRUPTIBLE);
+		timeout = schedule_timeout(timeout);
+	}
+}
+#endif
+
 #if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,9)
 static inline unsigned long msleep_interruptible(unsigned int msecs)
 {
@@ -17,6 +29,13 @@ static inline unsigned long msleep_interruptible(unsigned int msecs)
 		timeout = schedule_timeout(timeout);
 	}
 	return jiffies_to_msecs(timeout);
+}
+#endif
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,9)
+static inline void ssleep(unsigned int seconds)
+{
+	msleep(seconds * 1000);
 }
 #endif
 
