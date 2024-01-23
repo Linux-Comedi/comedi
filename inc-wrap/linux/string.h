fb40f6473e150396a373dd329abb488e52d51301
Ian Abbott <abbotti@mev.co.uk>
2024-01-23 11:50:21 +0000

    inc-wrap/linux/string.h: Add strscpy() compatibility for pre-4.3 kernels
    strscpy() was added in Linux kernel 4.3.  Define a compatible function
    for earlier kernels.  This will allow strlcpy() calls to be replaced
    with strscpy() in Comedi because strlcpy() is being removed from the 6.8
    kernel.

---
+++ inc-wrap/linux/string.h
@@ -0,0 +1,41 @@
+
+#ifndef COMEDI_COMPAT_LINUX_STRING_H
+#define COMEDI_COMPAT_LINUX_STRING_H
+
+#include <linux/version.h>
+#include <linux/types.h>
+#include <linux/errno.h>
+
+#include_next <linux/string.h>
+
+/*
+ * strscpy() was introduced in Linux kernel 4.3.0.  Define a compatibility
+ * function for earlier kernels.
+ *
+ * Note that callers of strscpy between kernel versions 4.3.0 and 4.15.x
+ * will get a warning about ignoring the return value of strscpy, but that
+ * warning was dropped from later kernel versions.
+ */
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4,3,0)
+
+#undef strscpy
+#define strscpy(d, s, n) comedi_strscpy(d, s, n)
+
+static inline ssize_t comedi_strscpy(char *d, const char *s, size_t n)
+{
+	const char *s_orig = s;
+	char ch = 1;
+
+	if (n) {
+		for (ch = *s; --n && ch; ch = *s) {
+			*d++ = ch;
+			s++;
+		}
+		*d = '\0';
+	}
+	return ch ? -E2BIG : (ssize_t)(s - orig_s);
+}
+
+#endif
+
+#endif /* COMEDI_COMPAT_LINUX_STRING_H */
