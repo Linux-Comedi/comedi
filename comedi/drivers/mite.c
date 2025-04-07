0dd06243c9137e5964f62a167a8e701904a1db0f
Ian Abbott <abbotti@mev.co.uk>
2025-04-07 15:47:53 +0100

    mite: Add MODULE_AUTHOR() and MODULE_DESCRIPTION()
    Builds for recent kernels issue a warning if MODULE_DESCRIPTION() is
    missing.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>

--- comedi/drivers/mite.c
+++ comedi/drivers/mite.c
@@ -61,6 +61,8 @@
 #define PCI_DAQ_SIZE		4096
 #define PCI_DAQ_SIZE_660X       8192
 
+MODULE_AUTHOR("Comedi https://www.comedi.org");
+MODULE_DESCRIPTION("Comedi helper for NI Mite PCI interface chip");
 MODULE_LICENSE("GPL");
 
 struct mite_struct *mite_devices = NULL;
