3bbe19587b8f10bc4d0441b13a59f832602b0d39
Ian Abbott <abbotti@mev.co.uk>
2025-06-13 14:28:22 +0100

    inc-wrap/linux/timer.h: Fix for Linux kernel 6.16 compatibility
    Linux kernel 6.16 renamed the `from_timer` macro to
    `timer_container_of`, but we tested for the presence of `from_timer` to
    decide whether to define the `timer_setup` function for compatibility
    with kernels prior to 4.14.
    
    Only define the `timer_setup` function if neither `timer_container_of`
    nor `from_timer` was defined.
    
    Define `timer_container_of` if missing.
    
    Keep the definition of the `from_timer` macro if missing because some
    Comedi drivers are still using it (but not for much longer!).
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>

--- inc-wrap/linux/timer.h
+++ inc-wrap/linux/timer.h
@@ -9,7 +9,18 @@
 
 #include_next <linux/timer.h>
 
+/*
+ * from_timer() was added in kernel version 4.14.0 at the same time as
+ * timer_setup().  from_timer() was renamed to timer_container_of() in
+ * kernel version 6.16.
+ */
+#ifndef timer_container_of
 #ifndef from_timer
+
+/*
+ * Neither the timer_container_of() macro nor the from_timer() macro was
+ * defined, so assume timer_setup() function wasn't defined either.
+ */
 
 static inline void timer_setup(struct timer_list *timer,
 			       void (*callback)(struct timer_list *),
@@ -21,9 +32,19 @@ static inline void timer_setup(struct timer_list *timer,
 	init_timer(timer);
 }
 
+#endif /* from_timer */
+
+#define timer_container_of(var, callback_timer, timer_fieldname) \
+	container_of(callback_timer, typeof(*var), timer_fieldname)
+#endif /* timer_container_of */
+
+#ifndef from_timer
+/*
+ * Define from_timer for backwards compatibility until callers updated to
+ * call timer_container_of instead.
+ */
 #define from_timer(var, callback_timer, timer_fieldname) \
 	container_of(callback_timer, typeof(*var), timer_fieldname)
-
 #endif /* from_timer */
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(6,2,0)
