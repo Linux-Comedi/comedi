a3197f99818b86631c9f30736e89d35e8523dc51
Ian Abbott <abbotti@mev.co.uk>
2025-11-03 13:25:15 +0000

    amplc_dio200: Add KERN_CONT to printk calls where necessary
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>

--- comedi/drivers/amplc_dio200.c
+++ comedi/drivers/amplc_dio200.c
@@ -2104,19 +2104,19 @@ static int dio200_attach(comedi_device * dev, comedi_devconfig * it)
 
 	printk(KERN_INFO "comedi%d: %s ", dev->minor, dev->board_name);
 	if (thisboard->bustype == isa_bustype) {
-		printk("(base %#lx) ", iobase);
+		printk(KERN_CONT "(base %#lx) ", iobase);
 	} else {
 #ifdef COMEDI_CONFIG_PCI
-		printk("(pci %s) ", pci_name(pci_dev));
+		printk(KERN_CONT "(pci %s) ", pci_name(pci_dev));
 #endif
 	}
 	if (irq) {
-		printk("(irq %u%s) ", irq, (dev->irq ? "" : " UNAVAILABLE"));
+		printk(KERN_CONT "(irq %u%s) ", irq, (dev->irq ? "" : " UNAVAILABLE"));
 	} else {
-		printk("(no irq) ");
+		printk(KERN_CONT "(no irq) ");
 	}
 
-	printk("attached\n");
+	printk(KERN_CONT "attached\n");
 
 	return 1;
 }
