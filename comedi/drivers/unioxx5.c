d188e6e8b581d05feb7c45358bd32a4083ae2266
Ian Abbott <abbotti@mev.co.uk>
2025-11-04 11:39:18 +0000

    unioxx5: Add KERN_CONT or newline to printk calls where necessary
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>

--- comedi/drivers/unioxx5.c
+++ comedi/drivers/unioxx5.c
@@ -320,11 +320,11 @@ static int __unioxx5_subdev_init(comedi_subdevice * subdev, int subdev_iobase,
 		} else
 			usp->usp_module_type[i] = inb(subdev_iobase + 6);
 
-		printk(" [%d] 0x%02x |", i, usp->usp_module_type[i]);
+		printk(KERN_CONT " [%d] 0x%02x |", i, usp->usp_module_type[i]);
 		comedi_udelay(1);
 	}
 
-	printk("\n");
+	printk(KERN_CONT "\n");
 
 	/* initial subdevice for digital or analog i/o */
 	subdev->type = COMEDI_SUBD_DIO;
@@ -454,7 +454,7 @@ static int __unioxx5_analog_read(unioxx5_subd_priv * usp, lsampl_t * data,
 	/* defining if given module can work on input */
 	if (usp->usp_module_type[module_no] & MODULE_OUTPUT_MASK) {
 		printk(KERN_ERR
-			"comedi%d: module in position %d with id 0x%02x is for output only",
+			"comedi%d: module in position %d with id 0x%02x is for output only\n",
 			minor, module_no, usp->usp_module_type[module_no]);
 		return 0;
 	}
