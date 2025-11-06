34b0366eaf51e39c7c2cb685ba5d35a7cabae80d
Ian Abbott <abbotti@mev.co.uk>
2025-11-06 10:55:58 +0000

    comedi_internal.h: Remove unneeded declarations
    Remove unneeded declarations of `struct class *comedi_class` and `struct
    file_operations comedi_fops` from the shared internal header file.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>

--- comedi/comedi_internal.h
+++ comedi/comedi_internal.h
@@ -2,8 +2,6 @@
 #ifndef _COMEDI_INTERNAL_H
 #define _COMEDI_INTERNAL_H
 
-extern struct class *comedi_class;
-extern const struct file_operations comedi_fops;
 extern COMEDI_MODULE_PARAM_BOOL_T comedi_autoconfig;
 
 #endif //_COMEDI_INTERNAL_H
