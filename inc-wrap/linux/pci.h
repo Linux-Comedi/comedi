0844e762b88c0c09f9e1d98e54fe3285edb85f29
Ian Abbott <abbotti@mev.co.uk>
2025-11-21 14:25:43 +0000

    inc-wrap/linux/pci.h: Redefine pci_request_region and pci_request_regions
    For Linux kernel versions prior to 2.6.17, define
    comedi_pci_request_region() to call pci_request_region() with the const
    qualifier removed from the resource name string, and define
    comedi_pci_request_regions() to call pci_request_regions() in the same
    way.  Define pci_request_region() as a macro calling
    comedi_pci_request_region() and define pci_request_regions() as a macro
    calling comedi_pci_request_regions().  This avoids some compiler
    warnings about const qualifiers being discarded when calling
    pci_request_region() or pci_request_regions() for these kernels.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>

--- inc-wrap/linux/pci.h
+++ inc-wrap/linux/pci.h
@@ -6,10 +6,39 @@
 #define COMEDI_COMPAT__LINUX__PCI_H__INCLUDED__
 
 #include_next <linux/pci.h>
+#include <linux/version.h>
 
 #ifndef DEFINE_PCI_DEVICE_TABLE
 #define DEFINE_PCI_DEVICE_TABLE(_table) \
 	struct pci_device_id _table[]
+#endif
+
+/*
+ * Prior to kernel version 2.6.17, the const qualifier is missing from the
+ * res_name parameter of pci_request_region() and pci_request_regions().
+ * Redefine them as macros discarding the const qualifier for these kernel
+ * versions.
+ */
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,17)
+
+static inline int
+comedi_pci_request_region(struct pci_dev *pdev, int bar, const char *res_name)
+{
+	return pci_request_region(pdev, bar, (char *)res_name);
+}
+#undef pci_request_region
+#define pci_request_region(pdev, bar, res_name) \
+	comedi_pci_request_region(pdev, bar, res_name)
+
+static inline int
+comedi_pci_request_regions(struct pci_dev *pdev, const char *res_name)
+{
+	return pci_request_regions(pdev, (char *)res_name);
+}
+#undef pci_request_regions
+#define pci_request_regions(pdev, res_name) \
+	comedi_pci_request_regions(pdev, res_name)
+
 #endif
 
 #endif
