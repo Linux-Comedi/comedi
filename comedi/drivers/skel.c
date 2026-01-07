2065d1608449d76ca5bb8514f556a448de46832d
Ian Abbott <abbotti@mev.co.uk>
2026-01-07 12:38:41 +0000

    skel: Prefer the CMDF_ macros for command flag
    Use the CMDF_ macros for command flags instead of the corresponding
    TRIG_ flags.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>

--- comedi/drivers/skel.c
+++ comedi/drivers/skel.c
@@ -477,14 +477,14 @@ static int skel_ai_cmdtest(comedi_device * dev, comedi_subdevice * s,
 	if (cmd->scan_begin_src == TRIG_TIMER) {
 		tmp = cmd->scan_begin_arg;
 		skel_ns_to_timer(&cmd->scan_begin_arg,
-			cmd->flags & TRIG_ROUND_MASK);
+			cmd->flags & CMDF_ROUND_MASK);
 		if (tmp != cmd->scan_begin_arg)
 			err++;
 	}
 	if (cmd->convert_src == TRIG_TIMER) {
 		tmp = cmd->convert_arg;
 		skel_ns_to_timer(&cmd->convert_arg,
-			cmd->flags & TRIG_ROUND_MASK);
+			cmd->flags & CMDF_ROUND_MASK);
 		if (tmp != cmd->convert_arg)
 			err++;
 		if (cmd->scan_begin_src == TRIG_TIMER &&
