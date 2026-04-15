05585395d0136705e3873792a4b2c237252eb361
Ian Abbott <abbotti@mev.co.uk>
2026-04-15 15:57:46 +0100

    das800: Reformat initializers for ranges
    Use BIP_RANGE() to specify symmetrical bipolar ranges and UNI_RANGE() to
    specify unipolar ranges.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>

--- comedi/drivers/das800.c
+++ comedi/drivers/das800.c
@@ -116,66 +116,66 @@ typedef struct das800_board_struct {
 static const comedi_lrange range_das800_ai = {
 	1,
 	{
-		RANGE(-5, 5),
+		BIP_RANGE(5),
 	},
 };
 
 static const comedi_lrange range_das801_ai = {
 	9,
 	{
-		RANGE(-5, 5),
-		RANGE(-10, 10),
-		RANGE(0, 10),
-		RANGE(-0.5, 0.5),
-		RANGE(0, 1),
-		RANGE(-0.05, 0.05),
-		RANGE(0, 0.1),
-		RANGE(-0.01, 0.01),
-		RANGE(0, 0.02),
+		BIP_RANGE(5),
+		BIP_RANGE(10),
+		UNI_RANGE(10),
+		BIP_RANGE(0.5),
+		UNI_RANGE(1),
+		BIP_RANGE(0.05),
+		UNI_RANGE(0.1),
+		BIP_RANGE(0.01),
+		UNI_RANGE(0.02),
 	},
 };
 
 static const comedi_lrange range_cio_das801_ai = {
 	9,
 	{
-		RANGE(-5, 5),
-		RANGE(-10, 10),
-		RANGE(0, 10),
-		RANGE(-0.5, 0.5),
-		RANGE(0, 1),
-		RANGE(-0.05, 0.05),
-		RANGE(0, 0.1),
-		RANGE(-0.005, 0.005),
-		RANGE(0, 0.01),
+		BIP_RANGE(5),
+		BIP_RANGE(10),
+		UNI_RANGE(10),
+		BIP_RANGE(0.5),
+		UNI_RANGE(1),
+		BIP_RANGE(0.05),
+		UNI_RANGE(0.1),
+		BIP_RANGE(0.005),
+		UNI_RANGE(0.01),
 	},
 };
 
 static const comedi_lrange range_das802_ai = {
 	9,
 	{
-		RANGE(-5, 5),
-		RANGE(-10, 10),
-		RANGE(0, 10),
-		RANGE(-2.5, 2.5),
-		RANGE(0, 5),
-		RANGE(-1.25, 1.25),
-		RANGE(0, 2.5),
-		RANGE(-0.625, 0.625),
-		RANGE(0, 1.25),
+		BIP_RANGE(5),
+		BIP_RANGE(10),
+		UNI_RANGE(10),
+		BIP_RANGE(2.5),
+		UNI_RANGE(5),
+		BIP_RANGE(1.25),
+		UNI_RANGE(2.5),
+		BIP_RANGE(0.625),
+		UNI_RANGE(1.25),
 	},
 };
 
 static const comedi_lrange range_das80216_ai = {
 	8,
 	{
-		RANGE(-10, 10),
-		RANGE(0, 10),
-		RANGE(-5, 5),
-		RANGE(0, 5),
-		RANGE(-2.5, 2.5),
-		RANGE(0, 2.5),
-		RANGE(-1.25, 1.25),
-		RANGE(0, 1.25),
+		BIP_RANGE(10),
+		UNI_RANGE(10),
+		BIP_RANGE(5),
+		UNI_RANGE(5),
+		BIP_RANGE(2.5),
+		UNI_RANGE(2.5),
+		BIP_RANGE(1.25),
+		UNI_RANGE(1.25),
 	},
 };
 
